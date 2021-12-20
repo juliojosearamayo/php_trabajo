1d5a111a42cffa0c8a2d31ec5da26dccc5617660
Julio Jose A.G <juliojosearamayo@gmail.com>
2021-12-19 22:21:57 -0400

    Create contacto.php

---
+++ contacto.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Inicio</title>
+	<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+<body>
+
+	<header class="main-header">
+		<div class="container clearfix">
+			<h1 class="name"><a href="#">Optica ROVER</a></h1>
+			<ul class="main-nav">
+				<li><a href="index.php">Inicio</a></li>
+				<li><a href="galeria.php">Galeria</a></li>
+				<li><a href="contacto.php">Contacto</a></li>
+				<li><a href="tarea2.php">Tarea2</a></li>
+                <li><a href="tarea3.php">Tarea3</a></li>
+			</ul>
+		</div>
+
+		
+	</header><!--/.main-header-->
+
+	<div class="banner">
+
+		<img src="img/a8.jpg" class="img-fluid" alt="...">
+		
+	</div>
+
+	<div class="container">
+	<form class="row g-3">
+		<h5>Póngase en contacto con nosotros y le ayudaremos rápidamente...</h5>
+		<div class="row">
+			<div class="col-md-2" style="background-color: #dedef8;box-shadow: inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+				<h4>Telefono</h4>
+				<p>65893526</p>
+				<h4>Dirección</h4>
+				<p>Prolongación avenida Brasil frente a la caja petrolera</p>
+			</div>
+			<div class="col-md-10" style="background-color: #dedef8;box-shadow: inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+				
+				<div class="row">
+					
+					<div class="col-md-10" style="background-color: #B18904; box-shadow: inset 1px -1px 1px #444, inset -1px 1px 1px #444;">
+						<div class="mb-3">
+							<label for="exampleFormControlInput1" class="form-label">Email address</label>
+							<input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+						</div>
+						<div class="mb-3">
+							<label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
+							<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+						</div>
+						<div class="mb-3">
+							
+							<input class="btn btn-primary" type="submit" value="Enviar">
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</form>
+	</div>
+	
+
+	
+
+	<footer class="main-footer">
+		<span>&copy; 2021 CRECE</span>
+	</footer>
+
+
+	<script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
