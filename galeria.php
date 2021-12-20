fe818d902fdb2c6bbc40c370a273eac1ad08e872
Julio Jose A.G <juliojosearamayo@gmail.com>
2021-12-19 22:23:07 -0400

    Create galeria.php

---
+++ galeria.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Inicio</title>
+	<link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+	<header class="main-header">
+		<div class="container clearfix">
+			<h1 class="name"><a href="#">Optica ROVER</a></h1>
+			<ul class="main-nav">
+				<li><a href="index.php">Inicio</a></li>
+				<li><a href="galeria.php">Galeria</a></li>
+				<li><a href="contacto.php">Contacto</a></li>
+				<li><a href="tarea2.php">Tarea2</a></li>
+                <li><a href="tarea3.php">Tarea3</a></li>
+			</ul>
+		</div>
+
+		
+
+		
+	</header><!--/.main-header-->
+	<section id="galeria" class="container">
+		<div class="text-center pt-5">
+			<h1>Galeria</h1>
+			<p></p>
+		</div>
+		<div class="row">
+			<div class="col-lg-4 col-md-6 col-sm-12"><img src="img/a3.jpg" alt=""></div>
+			<div class="col-lg-4 col-md-6 col-sm-12"><img src="img/a4.jpg" alt=""></div>
+			<div class="col-lg-4 col-md-6 col-sm-12"><img src="img/a5.jpg" alt=""></div>
+			<div class="col-lg-4 col-md-6 col-sm-12"><img src="img/a6.jpg" alt=""></div>
+			<div class="col-lg-4 col-md-6 col-sm-12"><img src="img/a7.jpg" alt=""></div>
+			<div class="col-lg-4 col-md-6 col-sm-12"><img src="img/a8.jpg" alt=""></div>
+		</div>
+	</section>
+	
+	<footer class="main-footer">
+		<span>&copy; 2021 CRECE</span>
+	</footer>
+
+
+	<script src="js/jquery-3.6.0.min.js"></script>
+    <script src="js/popper.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
+</body>
+</html>
